938ea901246a09125dff3684549601bb772dc8c0
javiOlaya <jjosaez@gmail.com>
2019-10-30 07:11:12 +0100

    proyecto terminado

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 	Scanner scanner = new Scanner(System.in);
+        int k=0;
         System.out.println("Programa para comprobar Palíndromos ");
         char[] palabra=(scanner.nextLine()).toCharArray();
         char[] prueba= new char[palabra.length];
         for(int i=0;i<palabra.length;i++) prueba[(palabra.length-1)-i]=palabra[i];
         System.out.print("palabra invertida :");
         System.out.println(prueba);
+
+        for (int j=0;j<palabra.length;j++) {
+            if (palabra[j] == prueba[j]) k++;
+            else break;
+        }
+          String resultado=(k==(palabra.length))?"Es un palindromo":"No es un palindromo";
+            System.out.println(resultado);
+
+
     }
 }
